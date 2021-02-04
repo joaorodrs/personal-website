24e16fb3877d330f1683c8fe5be2976ca4fa3356
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2021-02-04 18:47:31 -0300

    Initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { useState } from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>João Paulo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
